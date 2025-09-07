ca1edddf6fb258082d5050f323ec1678c3b6f44d
BakaAhoManuke <nattawut.klinthuesin@gmail.com>
2025-09-07 15:15:08 +0700

    move 2 Problem SQL

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++
@@ -1,7 +0,0 @@
-# Write your MySQL query statement below
-SELECT ROUND(AVG(dOut.order_date = dOut.customer_pref_delivery_date) * 100, 2) immediate_percentage
-FROM Delivery dOut JOIN (
-    SELECT customer_id, MIN(order_date) first_order
-    FROM Delivery
-    GROUP BY customer_id
-) dIn ON dOut.customer_id = dIn.customer_id AND dOut. order_date = dIn.first_order;
